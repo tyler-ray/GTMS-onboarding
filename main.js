378120c9876d8061b8f08fcf4edade69e599c2ab
Tyler Ray <jtray@outlook.es>
2019-06-04 15:56:28 -0600

    rough draft

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 // var myDropzone = new myDropzone("div#dropzone",{url: "/file/post"});
-$(document).ready(()=>{
-    $($("div#myDropzone").dropzone({ url: "/file/post" }))
-    $($("img#myDropzone2").dropzone({ url: "/file/post" }))
-
-})
